8b5624586314a12cac064c46e0a44c83ac469181
jjYBdx4IL <jjYBdx4IL@github.com>
2017-04-15 05:11:19 +0200

    add edge detection filter and live param adjustment window

--- javacv-webcam/src/main/java/com/github/jjYBdx4IL/graphics/examples/JavaCVWebCam.java
+++ javacv-webcam/src/main/java/com/github/jjYBdx4IL/graphics/examples/JavaCVWebCam.java
@@ -1,7 +1,13 @@
 package com.github.jjYBdx4IL.graphics.examples;
 
 import org.bytedeco.javacpp.opencv_core;
+import static org.bytedeco.javacpp.opencv_core.CV_32F;
+import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
 import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_imgproc;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_RGB2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.Sobel;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
@@ -17,27 +23,39 @@ public class JavaCVWebCam implements Runnable {
     //final int INTERVAL=1000;///you may use interval
     IplImage image;
     CanvasFrame canvas = new CanvasFrame("Web Cam");
+    ParamInputFrame params = new ParamInputFrame();
 
     public JavaCVWebCam() {
         canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        params.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
     }
 
     @Override
     public void run() {
         FrameGrabber grabber = new VideoInputFrameGrabber(0); // 1 for next camera
-        OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+        //OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+        OpenCVFrameConverter.ToMat cvt = new OpenCVFrameConverter.ToMat();
         int i = 0;
         try {
             grabber.start();
             IplImage img;
             while (true) {
                 Frame frame = grabber.grab();
-                img = converter.convert(frame);
+                img = cvt.convertToIplImage(frame);
                 if (img != null) {
-                    opencv_core.cvFlip(img, img, 1);// l-r = 90_degrees_steps_anti_clockwise
+                    //Mat frameMat = cvt.convert(frame);
+                    //Mat sobelX = new Mat();
+                    //Sobel(frameMat, sobelX, CV_32F, 1, 0);
+                    IplImage imgGray = opencv_core.cvCreateImage(opencv_core.cvSize(frame.imageWidth, frame.imageHeight), IPL_DEPTH_8U, 1);
+                    opencv_imgproc.cvCvtColor(img, imgGray, CV_RGB2GRAY);
+                    
+                    //opencv_core.cvFlip(img, img, 1);// l-r = 90_degrees_steps_anti_clockwise
+                    //opencv_imgproc.cvSobel(img, img, 1, 1);
+                    opencv_imgproc.cvCanny(imgGray, imgGray, params.threshold1(), params.threshold2(), params.apertureSize());
                     //opencv_imgcodecs.cvSaveImage((i++)+"-aa.jpg", img);
                     // show image on window
-                    canvas.showImage(frame);
+                    canvas.showImage(cvt.convert(imgGray));
+                    //canvas.showImage(cvt.convert(sobelX));
                 }
                 //Thread.sleep(INTERVAL);
             }
---
+++ javacv-webcam/src/main/java/com/github/jjYBdx4IL/graphics/examples/ParamInputFrame.java
@@ -0,0 +1,86 @@
+package com.github.jjYBdx4IL.graphics.examples;
+
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author jjYBdx4IL
+ */
+public class ParamInputFrame extends JFrame implements ActionListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ParamInputFrame.class);
+
+    protected final AtomicInteger threshold1Value = new AtomicInteger(Float.floatToIntBits(1f));
+    protected final AtomicInteger threshold2Value = new AtomicInteger(Float.floatToIntBits(100f));
+    protected final AtomicInteger apertureSizeValue = new AtomicInteger(3);
+
+    protected final JTextField threshold1 = new JTextField(Float.toString(Float.intBitsToFloat(threshold1Value.get())));
+    protected final JTextField threshold2 = new JTextField(Float.toString(Float.intBitsToFloat(threshold2Value.get())));
+    protected final JTextField apertureSize = new JTextField(Integer.toString(apertureSizeValue.get()));
+
+    public ParamInputFrame() {
+        super("params");
+
+        GridBagConstraints c = new GridBagConstraints();
+        GridBagLayout layout = new GridBagLayout();
+        Container container = getContentPane();
+        container.setLayout(layout);
+
+        c.gridx = 0;
+        c.weightx = .5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        
+        c.gridy = 0;
+        container.add(threshold1, c);
+        c.gridy++;
+        container.add(threshold2, c);
+        c.gridy++;
+        container.add(apertureSize, c);
+
+        threshold1.addActionListener(this);
+        threshold2.addActionListener(this);
+        apertureSize.addActionListener(this);
+
+        pack();
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        LOG.info(e.toString());
+        try {
+            if (e.getSource() == threshold1) {
+                threshold1Value.set(Float.floatToIntBits(Float.parseFloat(threshold1.getText())));
+            }
+            if (e.getSource() == threshold2) {
+                threshold2Value.set(Float.floatToIntBits(Float.parseFloat(threshold2.getText())));
+            }
+            if (e.getSource() == apertureSize) {
+                apertureSizeValue.set(Integer.parseInt(threshold2.getText()));
+            }
+        } catch (NumberFormatException ex) {
+            LOG.error("", ex);
+        }
+    }
+
+    public float threshold1() {
+        return Float.intBitsToFloat(threshold1Value.get());
+    }
+
+    public float threshold2() {
+        return Float.intBitsToFloat(threshold2Value.get());
+    }
+
+    public int apertureSize() {
+        return apertureSizeValue.get();
+    }
+}
