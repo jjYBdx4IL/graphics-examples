9d8b857d747096d7f050193fc9deefe06bb1e3b7
jjYBdx4IL <jjYBdx4IL@github.com>
2017-04-21 09:16:54 +0200

    bugfix

--- javacv-webcam/src/main/java/com/github/jjYBdx4IL/graphics/examples/filterdefs/DilateFilter.java
+++ javacv-webcam/src/main/java/com/github/jjYBdx4IL/graphics/examples/filterdefs/DilateFilter.java
@@ -1,9 +1,16 @@
 package com.github.jjYBdx4IL.graphics.examples.filterdefs;
 
+import org.bytedeco.javacpp.opencv_core;
+
 /**
  *
  * @author jjYBdx4IL
  */
 public class DilateFilter extends AbstractFilter {
+
+    @Override
+    public opencv_core.IplImage process(opencv_core.IplImage img) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
